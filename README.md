396bd05de790f45d1aa059d4e90f3bdfd8f63702
JoeHall <halljoe312@gmail.com>
2022-11-18 12:58:37 +0000

    Random policy play implemented.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rl_group1
-Repo for Reinforcement Learning group 1 project 
+Repo for Reinforcement Learning group 1 project.
+Requirements:
+Python version = 3.9
+Gym[atari] libaries to be installed. Done via https://www.youtube.com/watch?v=e3DyCg0fgx0.
+
--- skiing.py
+++ skiing.py
@@ -1,4 +1,28 @@
 import gym
+import time
 from gym.utils.play import play
-env = gym.make('Skiing-v4')
-play(env)
+
+def createEnvironment():
+    env = gym.make('Skiing-v4')
+    return env
+
+# Runs environment using random policy
+def runEnvironment(env):
+    observation = env.reset()# Reset the environment
+    termination=False
+    while not termination:
+        env.render('human') # Render the environment
+        observation, reward, termination, info = doRandomAction(env)
+        time.sleep(0.01) # sleep between each timestep
+    env.close()
+
+def doRandomAction(env):
+    return env.step(env.action_space.sample())
+
+# Human play environment
+def playEnvironment(env):
+    play(env)
+
+env = createEnvironment()
+runEnvironment(env)
+#playEnvironment(env)
