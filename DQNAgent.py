1618e703ec76e5afd13af457ac34955b55ccf8d6
jsoor24 <jeevans@protonmail.com>
2022-12-06 11:44:18 +0000

    Generate episode for DQNAgent

--- DQNAgent.py
+++ DQNAgent.py
@@ -9,6 +9,19 @@ class DQNAgent():
         return
 
     def generateEpisode(self, render=False):
-        # some code
-        return
-
+        env = self.env
+        observation = env.reset()
+        episode = []
+        terminal = False
+        sum_of_reward=0
+        while not terminal:
+            # If been told to render, render before every action so user can see simulation.
+            if(render):
+                env.render()
+            action = self.policy(observation)
+            n_observation, reward, terminal, info = env.step(action)
+            sum_of_reward=sum_of_reward+reward
+            episode.append((observation,action,reward))
+            observation = n_observation
+        env.close()
+        return episode, sum_of_reward
