2c096ca0a7ba19129feb423daa24f0c561442e45
jsoor24 <jeevans@protonmail.com>
2022-12-06 11:37:45 +0000

    Running cartpole env

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from DQNAgent import DQNAgent
 import pygame
 
 def play():
-    skiing.playEnvironment(mapping)
+    environment.playEnvironment(mapping)
 
 # Initialise skiing environment.
 mapping = {(pygame.K_LEFT,): 0, (pygame.K_RIGHT,): 1}
-environement = env('Skiing-v4')
+environment = env('CartPole-v1')
 # Initialise agent using environment.
-agent = DQNAgent(environement)
+agent = DQNAgent(environment)
 # Generate episode using agent.
 episode, reward = agent.generateEpisode()
+print(episode)
+print("\n")
+print(reward)
