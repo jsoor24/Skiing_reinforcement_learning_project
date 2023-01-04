f7c6c7295f607a448f11902e1bc53c3c85f471d9
jsoor24 <jeevans@protonmail.com>
2023-01-04 19:11:55 +0000

    Added nice print messages each iteration. Added comments to functions. Added code to go through time steps one at a time

--- Env.py
+++ Env.py
@@ -13,25 +13,49 @@ class Env:
         self.gym_env.render_mode = render_mode
         self.gym_env.env.obs_type = obs_type
         self.number_of_features=4
-    
+
     # Function to test feature extraction of skiing while implementing 
     def testFeatureExtraction(self):
         # Returns episode in data struct (list(trajectory), sum_reward) for testing
         episode = RandomAgent(self).generateEpisode()
-        states = random.choices(episode[0], k=10)
+
+        # For random time steps
+        # states = random.choices(episode[0], k=10)
+        # For x time steps at a time
+        states = []
+        for i in range(0, 100, 10):
+            states.append(episode[0][i])
+
+
         count=1
         p_state_obs = None
+        prev_flag = []
+        prev_pos = []
         for state in states:
             n_state_obs = state[0]
             # Data structure of objects:
             # dict(object_type, list( dict( pixel_coord, rgb_value)))
             features = self.features(p_state_obs, n_state_obs)
-            p_state_obs = n_state_obs
-            print()
+
+            if p_state_obs is not None:
+                p_objects, n_objects = self.detectObjects(p_state_obs, n_state_obs)
+                prev_flag = p_objects["pole"]
+                prev_pos = p_objects["player"][0]
+            else:
+                p_objects, n_objects = self.detectObjects(None, n_state_obs)
+
+            next_flag = n_objects["pole"]
+            next_pos = n_objects["player"][0]
+
+            print("Flags: ", prev_flag, " -> ", next_flag)
+            print("Player: ", prev_pos, " -> ", next_pos)
+            print("--------")
             print("Features for random state",count,":")
             print(features)
             print()
+
             self.plot2Observation(n_state_obs)
+            p_state_obs = n_state_obs
             count+=1
 
     def plot2Observation(self,observation):
@@ -56,15 +80,23 @@ class Env:
 
         start_poles_pos = p_obs_objects["pole"]
         end_poles_pos = n_obs_objects["pole"]
+
+        # Take care of scenario where 4 flags are disappearing from top of screen
+        # ignore flags above player
+        # play the game and double check
+
         n_of_start_poles = len(start_poles_pos)
         n_of_end_poles = len(end_poles_pos)
 
-        print("Poles in first obs ", start_poles_pos)
-        print("Poles in second obs", end_poles_pos)
-
         # This should never happen
-        if n_of_start_poles != 4 and n_of_start_poles != 2 and n_of_end_poles != 4 and n_of_end_poles != 2:
+        if n_of_start_poles != 4 and n_of_start_poles != 2:
             print("ERROR: Unexpected number of poles in observation")
+            print("Observed ", n_of_start_poles, " in first observation")
+            return 0, 0
+
+        if n_of_end_poles != 4 and n_of_end_poles != 2:
+            print("ERROR: Unexpected number of poles in observation")
+            print("Observed ", n_of_end_poles, " in second observation")
             return 0, 0
 
         # 4 scenarios can occur with the number of poles:
@@ -73,7 +105,7 @@ class Env:
         # (2, 4) 2 new poles in second observation (check dist between first poles in both observations)
         # (4, 2) 2 poles no longer visible in second observation (check dist between 'third pole' and 'first pole')
         if n_of_start_poles == 4 and n_of_end_poles == 2:
-            v_velocity = end_poles_pos[2][0] - start_poles_pos[0][0]
+            v_velocity = end_poles_pos[0][0] - start_poles_pos[2][0]
         else:
             v_velocity = end_poles_pos[0][0] - start_poles_pos[0][0]
 
@@ -84,30 +116,29 @@ class Env:
         # Get position of flags which are below player
         poles = n_obs_objects["pole"]
         while len(poles)>0:
-            flags = self.getFlagCenter(poles.pop(0), poles.pop(0))
+            flags = self.getFlagCentre(poles.pop(0), poles.pop(0))
             flag_h_pos=flags[1]-player_pos[1]
             flag_v_pos=flags[0]-player_pos[0]
             # Test if first flag set is next sub goal (not above player)
             if flag_v_pos>0:
-                print("Player pos: ",player_pos)
-                print("Flags pos: ", flags)
                 return flag_h_pos, flag_v_pos
         print("ERROR - CANNOT DETECT POLE POSITION.")
 
-    def getFlagCenter(self, first, second):
+    # Get centre point between two flags
+    def getFlagCentre(self, first, second):
         y = round((first[0]+second[0])/2)
         x = round((first[1]+second[1])/2)
         return y,x
 
 
-    # Function to get object center
-    def getObjectCenter(self, pixels_dict):
+    # Function to get object center from pixels
+    def getObjectCentre(self, pixels_dict):
         pixels = list(pixels_dict.items())
         r_pixel_cords = []
         c_pixel_cords = []
         for pixel in pixels:
             r_pixel_cords.append(pixel[0][0])
-            c_pixel_cords.append(pixel[0][1]) 
+            c_pixel_cords.append(pixel[0][1])
         length = len(pixels)
         return round(sum(r_pixel_cords)/length),round(sum(c_pixel_cords)/length)
 
@@ -116,10 +147,10 @@ class Env:
     def objectsToObjectCoords(self, objects):
         for ob_type in objects:
             object_list=objects[ob_type]
-            objects[ob_type]=[self.getObjectCenter(object) for object in object_list]
+            objects[ob_type]=[self.getObjectCentre(object) for object in object_list]
         return objects
 
-    # self.getObjectCenter to be used here so we only store object centers instead of whole pixels.
+    # Wrapper function to allow us to call with p = None
     def detectObjects(self, p_observation, n_observation):
         if p_observation is None:
             objects = self.identifyObjects(n_observation)
@@ -139,12 +170,12 @@ class Env:
         if p_obs_objects is None:
             h_velocity = 0
             v_velocity = 0
-        else: 
+        else:
             h_velocity, v_velocity = self.calculate_player_velocities(p_obs_objects, n_obs_objects)
         flag_h, flag_v = self.calculate_flag_distances(n_obs_objects)
         return h_velocity, v_velocity, flag_h, flag_v
-    
-    # We are performing feature extraction on every step of expience, need to do this for states used for training only! 
+
+    # We are performing feature extraction on every step of experience, need to do this for states used for training only!
     def step(self, action, p_observation):
         n_observation, reward, terminal, info = self.gym_env.step(action)
         return self.features(p_observation, n_observation), reward, terminal, info
@@ -180,7 +211,7 @@ class Env:
         else:
             return None
 
-    # Fetches all object pixels
+    # Fetches all pixels associated with one object/colour
     def identifyObjectPixels(self, observation):
         player_pixels = {}
         pole_pixels = {}
@@ -199,6 +230,8 @@ class Env:
                     tree_pixels[row, col] = pixel
         return player_pixels, pole_pixels, tree_pixels
 
+    # Finds neighbouring pixels for one object
+    # Given one blue dot for player identifies all player object pixels
     def findAdjacentPixelsRecursively(self, pixel_dict, row, col, object_pixels, ob_type):
         pixel = pixel_dict.pop((row, col), None)
         pixel_type = self.getObjectColourCategory(pixel)
@@ -211,6 +244,7 @@ class Env:
             self.findAdjacentPixelsRecursively(pixel_dict, row, col + 1, object_pixels, ob_type)
             self.findAdjacentPixelsRecursively(pixel_dict, row, col - 1, object_pixels, ob_type)
 
+    # Identifies objects from observation using pixel colour categories and populates dictionary
     def identifyObjects(self, observation):
         object_pixel_dicts = self.identifyObjectPixels(observation)
         objects = {'player': [object_pixel_dicts[0]]}
